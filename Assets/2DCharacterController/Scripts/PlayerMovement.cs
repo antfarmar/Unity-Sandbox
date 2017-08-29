4be008ca0e261c84bbe3ab328b9d631c37170c20
antfarmar <markfarrugiamail@gmail.com>
2017-08-29 01:44:16 -0400

    initial commit

---
+++ Assets/2DCharacterController/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // Uses Blend Trees to switch between animations.
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        // bool isWalking = 0 < (Mathf.Abs(inputX) + Mathf.Abs(inputY));
+        bool isWalking = inputX != 0 || inputY != 0;
+        animator.SetBool("isWalking", isWalking);
+
+        if (isWalking)
+        {
+            animator.SetFloat("x", inputX);
+            animator.SetFloat("y", inputY);
+            Vector3 direction = new Vector3(inputX, inputY, 0).normalized;
+            transform.Translate(Time.deltaTime * direction);
+        }
+    }
+}
